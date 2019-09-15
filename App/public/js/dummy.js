3b6c7b7914bab9a116e5e75538a68b2cb4a0ae32
rogdbowen <49526191+rogdbowen@users.noreply.github.com>
2019-09-15 17:52:11 -0400

    Changed App to app so that my file path worked properly.

---
+++ App/public/js/dummy.js
@@ -0,0 +1,19 @@
+var waitingApp = new Vue({
+  el: '#patientWaitingApp',
+  data: {
+    patients:{ }
+  },
+
+  methods:{
+    fetchPatients() {
+      fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(json=>{waitingApp.patients=json.results[0]});
+    }
+  },
+
+  created() {
+    this.fetchPatients();
+  }
+
+});
